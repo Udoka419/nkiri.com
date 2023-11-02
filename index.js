dc38916fbf4682965d08b66f685c6d5f4e4b0124
Udoka419 <nlememmanuel20@gmail.com>
2023-11-02 23:59:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("helllo");
+var noticex =document.getElementById("notice-close");
+var notice = document.querySelector(".notice");
+ 
+noticex.addEventListener('click' , () =>{
+    notice.style.display = "none";
+})
